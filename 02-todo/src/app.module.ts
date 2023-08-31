d0d6cf7ab94fe0f14658d4f08745f49b272e6244
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-08-31 08:41:06 -0400

    nest+graphql introduccion parte uno

---
+++ 02-todo/src/app.module.ts
@@ -0,0 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      // playground: false
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
