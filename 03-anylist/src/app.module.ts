61a5bcd2bb38d8bf4c0ddb79162cab85d55f66cb
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-09-07 08:38:00 -0300

    graphql + postgres parte dos

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -4,15 +4,28 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
       autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities:true,
     }),
     ItemsModule,
   ],
