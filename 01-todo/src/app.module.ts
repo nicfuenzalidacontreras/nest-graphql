ee1264d33a720e9e3b17283931cab3fec750f42d
Nicolás Fuenzalida Contreras <na.fuenzalida.contreras@gmail.com>
2024-08-23 16:25:30 -0400

    parte uno

--- 01-todo/src/app.module.ts
+++ 01-todo/src/app.module.ts
@@ -1,10 +1,21 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
